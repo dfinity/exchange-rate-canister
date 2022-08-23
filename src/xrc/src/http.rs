703cc42f63e83186d2f7dd6bda627362f6f2890a
Ryan Croote <ryan.croote@dfinity.org>
2022-08-23 14:36:16 -0400

    Adding the ability to make http requests

---
+++ src/xrc/src/http.rs
@@ -0,0 +1,123 @@
+use std::future::Future;
+
+use ic_cdk::{
+    api::call::CallResult,
+    export::{
+        candid::{self, CandidType},
+        serde::{Deserialize, Serialize},
+        Principal,
+    },
+};
+
+const IC_ENDPOINT: &str = "http_request";
+
+#[derive(CandidType, Clone, Deserialize, Debug, Eq, Hash, PartialEq, Serialize)]
+pub struct HttpHeader {
+    pub name: String,
+    pub value: String,
+}
+
+#[derive(Clone, Debug, PartialEq, CandidType, Eq, Hash, Serialize, Deserialize)]
+pub enum HttpMethod {
+    GET,
+    POST,
+    HEAD,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+struct CanisterHttpRequestArgs {
+    url: String,
+    max_response_bytes: Option<u64>,
+    headers: Vec<HttpHeader>,
+    body: Option<Vec<u8>>,
+    http_method: HttpMethod,
+    transform_method_name: Option<String>,
+}
+
+impl Default for CanisterHttpRequestArgs {
+    fn default() -> Self {
+        Self {
+            url: Default::default(),
+            max_response_bytes: Default::default(),
+            headers: vec![HttpHeader {
+                name: "User-Agent".to_string(),
+                value: "Exchange Rate Canister".to_string(),
+            }],
+            body: Default::default(),
+            http_method: HttpMethod::GET,
+            transform_method_name: Default::default(),
+        }
+    }
+}
+
+#[derive(CandidType, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CanisterHttpResponsePayload {
+    pub status: u64,
+    pub headers: Vec<HttpHeader>,
+    pub body: Vec<u8>,
+}
+
+pub struct CanisterHttpRequest {
+    args: CanisterHttpRequestArgs,
+    cycles: u64,
+}
+
+impl CanisterHttpRequest {
+    pub fn new() -> Self {
+        let args = CanisterHttpRequestArgs::default();
+        Self {
+            args,
+            cycles: Default::default(),
+        }
+    }
+
+    pub fn get(mut self, url: &str) -> Self {
+        self.url(url).method(HttpMethod::GET)
+    }
+
+    pub fn post(mut self, url: &str) -> Self {
+        self.url(url).method(HttpMethod::POST)
+    }
+
+    pub fn head(mut self, url: &str) -> Self {
+        self.url(url).method(HttpMethod::HEAD)
+    }
+
+    pub fn method(mut self, http_method: HttpMethod) -> Self {
+        self.args.http_method = http_method;
+        self
+    }
+
+    pub fn cycles(mut self, cycles: u64) -> Self {
+        self.cycles = cycles;
+        self
+    }
+
+    pub fn url(mut self, url: &str) -> Self {
+        self.args.url = String::from(url);
+        self
+    }
+
+    pub fn headers(mut self, headers: Vec<HttpHeader>) -> Self {
+        self.args.headers = headers;
+        self
+    }
+
+    pub fn body(mut self, body: Option<Vec<u8>>) -> Self {
+        self.args.body = body;
+        self
+    }
+
+    pub async fn send(self) -> CanisterHttpResponsePayload {
+        let args = candid::utils::encode_one(&self.args).unwrap();
+        let bytes = ic_cdk::api::call::call_raw(
+            Principal::management_canister(),
+            IC_ENDPOINT,
+            &args[..],
+            self.cycles,
+        )
+        .await
+        .unwrap();
+        candid::utils::decode_one(&bytes).unwrap()
+    }
+}
--- src/xrc/src/lib.rs
+++ src/xrc/src/lib.rs
@@ -1,10 +1,13 @@
+mod http;
 mod types;
 
 use ic_cdk::export::candid::candid_method;
 
 use jaq_core::{parse, Ctx, Definitions, RcIter, Val};
 use jaq_std::std;
-use serde_json::{from_str, Value};
+use serde_json::{from_slice, from_str, Value};
+
+use http::CanisterHttpRequest;
 
 #[ic_cdk_macros::query]
 #[candid_method(query)]
@@ -16,7 +19,35 @@ fn greet(name: String) -> String {
 #[candid_method(query)]
 fn extract_rate(response: String, filter: String) -> u64 {
     let input: Value = from_str(response.as_str()).unwrap();
+    let output = extract(input, &filter);
 
+    match output {
+        Val::Num(rc_number) => ((*rc_number).as_f64().unwrap() * 100.0) as u64,
+        _ => 0, // Return zero for now.
+    }
+}
+
+#[ic_cdk_macros::update]
+#[candid_method(update)]
+fn get_exchange_rate(_request: types::GetExchangeRateRequest) -> types::GetExchangeRateResult {
+    todo!()
+}
+
+#[ic_cdk_macros::update]
+#[candid_method(update)]
+async fn extract_from_http_request(url: String, filter: String) -> String {
+    extract_from_http_request_internal(&url, &filter).await
+}
+
+/// A wrapper for http_request as there is a bug with `candid_method` and async.
+async fn extract_from_http_request_internal(url: &str, filter: &str) -> String {
+    let payload = CanisterHttpRequest::new().get(&url).send().await;
+    let input = from_slice::<Value>(&payload.body).unwrap();
+    ic_cdk::println!("{}", input);
+    extract(from_slice::<Value>(&payload.body).unwrap(), filter).to_string()
+}
+
+fn extract(input: Value, filter: &str) -> Val {
     // Add required filters to the Definitions core.
     let mut definitions = Definitions::core();
 
@@ -30,7 +61,7 @@ fn extract_rate(response: String, filter: String) -> u64 {
 
     // Parse the filter in the context of the given definitions.
     let mut errs = Vec::new();
-    let f = parse::parse(&filter, parse::main()).0.unwrap();
+    let f = parse::parse(filter, parse::main()).0.unwrap();
     let f = definitions.finish(f, Vec::new(), &mut errs);
     assert_eq!(errs, Vec::new());
 
@@ -38,18 +69,7 @@ fn extract_rate(response: String, filter: String) -> u64 {
 
     // Extract the output.
     let mut out = f.run(Ctx::new([], &inputs), Val::from(input));
-    let output = out.next().unwrap().unwrap();
-
-    match output {
-        Val::Num(rc_number) => ((*rc_number).as_f64().unwrap() * 100.0) as u64,
-        _ => 0, // Return zero for now.
-    }
-}
-
-#[ic_cdk_macros::update]
-#[candid_method(update)]
-fn get_exchange_rate(_request: types::GetExchangeRateRequest) -> types::GetExchangeRateResult {
-    todo!()
+    out.next().unwrap().unwrap()
 }
 
 #[cfg(test)]
