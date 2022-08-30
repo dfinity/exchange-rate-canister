9af4436723021f3c0db307c86487bff2879a14e3
Ryan Croote <ryan.croote@dfinity.org>
2022-08-30 09:12:12 -0400

    Initial split of canister methods to main

--- src/xrc/src/lib.rs
+++ src/xrc/src/lib.rs
@@ -1,20 +1,18 @@
-#![deny(missing_docs)]
-
 //! The XRC provides a powerful exchange rate API, which can be leveraged by
 //! other applications, e.g., in the DeFi space.
 // TODO: expand on this documentation
 
 mod exchanges;
 mod http;
-mod jq;
-mod types;
+/// This module provides the ability to use `jq` filters on the returned
+/// response bodies.
+pub mod jq;
+/// This module provides the candid types.
+pub mod types;
 
 use exchanges::{Exchange, EXCHANGES};
-use ic_cdk::export::candid::candid_method;
 
-use jaq_core::Val;
-
-use http::CanisterHttpRequest;
+pub use http::CanisterHttpRequest;
 
 /// The arguments for the [call_exchanges] function.
 pub struct CallExchangesArgs {
@@ -111,65 +109,4 @@ async fn call_exchange(
             exchange: exchange.to_string(),
             error,
         })
-}
-
-#[ic_cdk_macros::query]
-#[candid_method(query)]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
-
-#[ic_cdk_macros::query]
-#[candid_method(query)]
-fn extract_rate(response: String, filter: String) -> u64 {
-    let output = jq::extract(response.as_bytes(), &filter).unwrap();
-
-    match output {
-        Val::Num(rc_number) => ((*rc_number).as_f64().unwrap() * 100.0) as u64,
-        _ => 0, // Return zero for now.
-    }
-}
-
-#[ic_cdk_macros::update]
-#[candid_method(update)]
-fn get_exchange_rate(_request: types::GetExchangeRateRequest) -> types::GetExchangeRateResult {
-    todo!()
-}
-
-#[ic_cdk_macros::update]
-#[candid_method(update)]
-async fn extract_from_http_request(url: String, filter: String) -> String {
-    let payload = CanisterHttpRequest::new().get(&url).send().await.unwrap();
-    jq::extract(&payload.body, &filter).unwrap().to_string()
-}
-
-#[ic_cdk_macros::update]
-#[candid_method(update)]
-async fn get_exchange_rates(request: types::GetExchangeRateRequest) -> Vec<u64> {
-    let (rates, _errors) = call_exchanges(CallExchangesArgs::from(request)).await;
-    rates
-}
-
-#[cfg(test)]
-mod test {
-    use std::path::PathBuf;
-
-    use super::*;
-
-    use ic_cdk::export::candid;
-
-    #[test]
-    fn check_candid_compatibility() {
-        candid::export_service!();
-        // Pull in the rust-generated interface and candid file interface.
-        let new_interface = __export_service();
-        let old_interface =
-            PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("xrc.did");
-
-        candid::utils::service_compatible(
-            candid::utils::CandidSource::Text(&new_interface),
-            candid::utils::CandidSource::File(old_interface.as_path()),
-        )
-        .expect("Service incompatibility found");
-    }
 }
