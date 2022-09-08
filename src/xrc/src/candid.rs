9515242014f44b1e9e9b267ae67c81070ef444c8
Thomas Locher <thomas.locher@dfinity.org>
2022-09-08 15:11:46 +0200

    Added more data to ExchangeRateInformation.

--- src/xrc/src/candid.rs
+++ src/xrc/src/candid.rs
@@ -21,36 +21,40 @@ pub struct Asset {
 /// The type the user sends when requesting a rate.
 #[derive(CandidType, Deserialize)]
 pub struct GetExchangeRateRequest {
-    /// An optional parameter used to find a rate at a specific time.
-    pub timestamp: Option<u64>,
-    /// The asset to be used as the starting asset. For example, using
-    /// ICP/USD, USD would be the quote asset.
-    pub quote_asset: Asset,
     /// The asset to be used as the resulting asset. For example, using
     /// ICP/USD, ICP would be the base asset.
     pub base_asset: Asset,
+    /// The asset to be used as the starting asset. For example, using
+    /// ICP/USD, USD would be the quote asset.
+    pub quote_asset: Asset,
+    /// An optional parameter used to find a rate at a specific time.
+    pub timestamp: Option<u64>,
 }
 
 /// Metadata information to give background on how the rate was determined.
 #[derive(CandidType, Deserialize)]
 pub struct ExchangeRateInformationMetadata {
-    /// The timestamp associated with the returned rate.
-    pub timestamp: u64,
     /// The number of exchanges queried to determine the results.
     pub number_of_queried_sources: u64,
-    /// The standard deviation of the received rates.
-    pub standard_deviation_permyriad: u64,
     /// The number rates successfully received from the queried sources.
     pub number_of_received_rates: u64,
+    /// The standard deviation of the received rates.
+    pub standard_deviation_permyriad: u64,
 }
 
 /// When a rate is determined, this struct is used to present the information
 /// to the user.
 #[derive(CandidType, Deserialize)]
 pub struct ExchangeRateInformation {
+    /// The base asset.
+    pub base_asset: Asset,
+    /// The quote asset.
+    pub quote_asset: Asset,
+    /// The timestamp associated with the returned rate.
+    pub timestamp: u64,
     /// The median rate from the received rates in permyriad.
     pub rate_permyriad: u64,
-    /// Metadata information to give background on how the rate was determined.
+    /// Metadata providing additional information about the exchange rate calculation.
     pub metadata: ExchangeRateInformationMetadata,
 }
 
