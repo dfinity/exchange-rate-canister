d619d2fc128bd94d4a421cee2a36617bb17c63db
Thomas Locher <thomas.locher@dfinity.org>
2022-09-12 14:29:53 +0200

    Renaming and clean-up.

--- src/xrc/src/candid.rs
+++ src/xrc/src/candid.rs
@@ -1,7 +1,7 @@
 use ic_cdk::export::candid::{CandidType, Deserialize};
 
 /// The enum defining the asset classes.
-#[derive(CandidType, Deserialize)]
+#[derive(CandidType, Debug, Deserialize)]
 pub enum AssetClass {
     /// The cryptocurrency asset class.
     Cryptocurrency,
@@ -10,7 +10,7 @@ pub enum AssetClass {
 }
 
 /// The struct capturing the symbol/code and class of an asset.
-#[derive(CandidType, Deserialize)]
+#[derive(CandidType, Debug, Deserialize)]
 pub struct Asset {
     /// The symbol/code of the asset.
     pub symbol: String,
@@ -21,35 +21,41 @@ pub struct Asset {
 /// The type the user sends when requesting a rate.
 #[derive(CandidType, Deserialize)]
 pub struct GetExchangeRateRequest {
-    /// An optional parameter used to find a rate at a specific time.
-    pub timestamp: Option<u64>,
-    /// The asset to be used as the starting asset. For example, using
-    /// ICP/USD, USD would be the quote asset.
-    pub quote_asset: Asset,
     /// The asset to be used as the resulting asset. For example, using
     /// ICP/USD, ICP would be the base asset.
     pub base_asset: Asset,
+    /// The asset to be used as the starting asset. For example, using
+    /// ICP/USD, USD would be the quote asset.
+    pub quote_asset: Asset,
+    /// An optional parameter used to find a rate at a specific time.
+    pub timestamp: Option<u64>,
 }
 
 /// Metadata information to give background on how the rate was determined.
-#[derive(CandidType, Deserialize)]
-pub struct ExchangeRateInformationMetadata {
+#[derive(CandidType, Debug, Deserialize)]
+pub struct ExchangeRateMetadata {
     /// The number of exchanges queried to determine the results.
     pub number_of_queried_sources: u64,
-    /// The spread among the rates that are close to the median (ignoring outliers).
-    pub spread: u64,
     /// The number rates successfully received from the queried sources.
     pub number_of_received_rates: u64,
+    /// The standard deviation of the received rates.
+    pub standard_deviation_permyriad: u64,
 }
 
 /// When a rate is determined, this struct is used to present the information
 /// to the user.
-#[derive(CandidType, Deserialize)]
-pub struct ExchangeRateInformation {
+#[derive(CandidType, Debug, Deserialize)]
+pub struct ExchangeRate {
+    /// The base asset.
+    pub base_asset: Asset,
+    /// The quote asset.
+    pub quote_asset: Asset,
+    /// The timestamp associated with the returned rate.
+    pub timestamp: u64,
     /// The median rate from the received rates in permyriad.
     pub rate_permyriad: u64,
-    /// Metadata information to give background on how the rate was determined.
-    pub metadata: ExchangeRateInformationMetadata,
+    /// Metadata providing additional information about the exchange rate calculation.
+    pub metadata: ExchangeRateMetadata,
 }
 
 // TODO: define more concrete error types instead of a generic when we have a
@@ -64,4 +70,4 @@ pub struct ExchangeRateError {
 }
 
 /// Short-hand for returning the result of a `get_exchange_rate` request.
-pub type GetExchangeRateResult = Result<ExchangeRateInformation, ExchangeRateError>;
+pub type GetExchangeRateResult = Result<ExchangeRate, ExchangeRateError>;
