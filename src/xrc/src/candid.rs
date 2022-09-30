6b7595b2cd5a9eac363a3c91342ae8f7ee9c7649
Thomas Locher <thomas.locher@dfinity.org>
2022-09-30 09:47:36 +0200

    Introduced the QueriedExchangeRate struct.

--- src/xrc/src/candid.rs
+++ src/xrc/src/candid.rs
@@ -1,3 +1,4 @@
+use crate::utils::median;
 use ic_cdk::export::candid::{CandidType, Deserialize};
 
 /// The enum defining the different asset classes.
@@ -34,10 +35,10 @@ pub struct GetExchangeRateRequest {
 /// Metadata information to give background on how the rate was determined.
 #[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
 pub struct ExchangeRateMetadata {
-    /// The number of exchanges queried to determine the results.
-    pub number_of_queried_sources: usize,
-    /// The number rates successfully received from the queried sources.
-    pub number_of_received_rates: usize,
+    /// The number of queried exchanges.
+    pub num_queried_sources: usize,
+    /// The number of rates successfully received from the queried sources.
+    pub num_received_rates: usize,
     /// The standard deviation of the received rates.
     pub standard_deviation_permyriad: u64,
 }
@@ -56,6 +57,79 @@ pub struct ExchangeRate {
     pub rate_permyriad: u64,
     /// Metadata providing additional information about the exchange rate calculation.
     pub metadata: ExchangeRateMetadata,
+}
+
+/// The received rates for a particular exchange rate request are stored in this struct.
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+pub struct QueriedExchangeRate {
+    /// The base asset.
+    pub base_asset: Asset,
+    /// The quote asset.
+    pub quote_asset: Asset,
+    /// The timestamp associated with the returned rate.
+    pub timestamp: u64,
+    /// The received rates in permyriad.
+    pub rates: Vec<u64>,
+    /// The number of queried exchanges.
+    pub num_queried_sources: usize,
+    /// The number of rates successfully received from the queried sources.
+    pub num_received_rates: usize,
+}
+
+impl std::ops::Mul for QueriedExchangeRate {
+    type Output = Self;
+
+    /// The function creates the product of two [QueriedExchangeRate] structs.
+    /// This is a meaningful operation if the quote asset of the first struct is
+    /// identical to the base asset of the second struct.
+    fn mul(self, other_rate: Self) -> Self {
+        let mut rates = vec![];
+        for own_value in self.rates {
+            for other_value in other_rate.rates.iter() {
+                rates.push(own_value.saturating_mul(*other_value));
+            }
+        }
+        Self {
+            base_asset: self.base_asset,
+            quote_asset: other_rate.quote_asset,
+            timestamp: self.timestamp,
+            rates,
+            num_queried_sources: self.num_queried_sources + other_rate.num_queried_sources,
+            num_received_rates: self.num_received_rates + other_rate.num_received_rates,
+        }
+    }
+}
+
+impl From<QueriedExchangeRate> for ExchangeRate {
+    fn from(rate: QueriedExchangeRate) -> Self {
+        ExchangeRate {
+            base_asset: rate.base_asset,
+            quote_asset: rate.quote_asset,
+            timestamp: rate.timestamp,
+            rate_permyriad: median(&rate.rates),
+            metadata: ExchangeRateMetadata {
+                num_queried_sources: rate.num_queried_sources,
+                num_received_rates: rate.num_received_rates,
+                standard_deviation_permyriad: 0,
+            },
+        }
+    }
+}
+
+impl QueriedExchangeRate {
+    #[allow(dead_code)]
+    /// The function returns the exchange rate with base asset and quote asset inverted.
+    pub(crate) fn inverted(&self) -> Self {
+        let inverted_rates: Vec<_> = self.rates.iter().map(|rate| 100_000_000 / rate).collect();
+        Self {
+            base_asset: self.quote_asset.clone(),
+            quote_asset: self.base_asset.clone(),
+            timestamp: self.timestamp,
+            rates: inverted_rates,
+            num_queried_sources: self.num_queried_sources,
+            num_received_rates: self.num_received_rates,
+        }
+    }
 }
 
 // TODO: define more concrete error types instead of a generic when we have a
