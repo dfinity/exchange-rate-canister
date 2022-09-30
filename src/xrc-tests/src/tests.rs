98c23fa6c581e9e6a3f9105827c4b04b5786a7fc
Ryan Croote <ryan.croote@dfinity.org>
2022-09-30 14:10:29 -0400

    Updating e2e test values

--- src/xrc-tests/src/tests.rs
+++ src/xrc-tests/src/tests.rs
@@ -88,8 +88,8 @@ fn can_successfully_retrieve_rate() {
     assert_eq!(exchange_rate.base_asset, request.base_asset);
     assert_eq!(exchange_rate.quote_asset, request.quote_asset);
     assert_eq!(exchange_rate.timestamp, timestamp);
-    assert_eq!(exchange_rate.metadata.num_queried_sources, 8);
-    assert_eq!(exchange_rate.metadata.num_received_rates, 8);
-    assert_eq!(exchange_rate.metadata.standard_deviation_permyriad, 27780);
-    assert_eq!(exchange_rate.rate_permyriad, 9973);
+    assert_eq!(exchange_rate.metadata.num_queried_sources, 12);
+    assert_eq!(exchange_rate.metadata.num_received_rates, 12);
+    assert_eq!(exchange_rate.metadata.standard_deviation_permyriad, 24279);
+    assert_eq!(exchange_rate.rate_permyriad, 9969);
 }
