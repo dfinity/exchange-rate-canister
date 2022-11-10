00a7debfb7a7f7c3f11a7a1e33ad7ba51b86fbf2
Thomas Locher <thomas.locher@dfinity.org>
2022-11-10 13:32:08 +0100

    Fixed standard deviation in e2e tests.

--- src/xrc-tests/src/tests.rs
+++ src/xrc-tests/src/tests.rs
@@ -125,7 +125,7 @@ fn can_successfully_retrieve_rate() {
     assert_eq!(exchange_rate.metadata.base_asset_num_received_rates, 7);
     assert_eq!(exchange_rate.metadata.quote_asset_num_queried_sources, 7);
     assert_eq!(exchange_rate.metadata.quote_asset_num_received_rates, 7);
-    assert_eq!(exchange_rate.metadata.standard_deviation_permyriad, 22754);
+    assert_eq!(exchange_rate.metadata.standard_deviation_permyriad, 22990);
     assert_eq!(exchange_rate.rate_permyriad, 9964);
 }
 
@@ -204,7 +204,7 @@ fn can_successfully_cache_rates() {
     assert_eq!(exchange_rate.metadata.base_asset_num_received_rates, 7);
     assert_eq!(exchange_rate.metadata.quote_asset_num_queried_sources, 7);
     assert_eq!(exchange_rate.metadata.quote_asset_num_received_rates, 7);
-    assert_eq!(exchange_rate.metadata.standard_deviation_permyriad, 22754);
+    assert_eq!(exchange_rate.metadata.standard_deviation_permyriad, 22990);
     assert_eq!(exchange_rate.rate_permyriad, 9964);
 
     let exchange_rate_2 = scenario_result
