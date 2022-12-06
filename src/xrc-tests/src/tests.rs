4e740f63d5b9508f0eb28354430f5d271721d519
Ryan Croote <ryan.croote@dfinity.org>
2022-12-06 09:47:34 -0500

    Sanitize the base and quote asset symbols to uppercase (#116)
    This PR adds some input sanitizing for the incoming request. The
    requested symbols should be uppercased as the canister only deals with
    currency symbols in uppercase unless formatted otherwise.

--- src/xrc-tests/src/tests.rs
+++ src/xrc-tests/src/tests.rs
@@ -78,11 +78,11 @@ fn can_successfully_retrieve_rate() {
     let request = GetExchangeRateRequest {
         timestamp: Some(timestamp),
         quote_asset: Asset {
-            symbol: "btc".to_string(),
+            symbol: "BTC".to_string(),
             class: AssetClass::Cryptocurrency,
         },
         base_asset: Asset {
-            symbol: "icp".to_string(),
+            symbol: "ICP".to_string(),
             class: AssetClass::Cryptocurrency,
         },
     };
@@ -141,11 +141,11 @@ fn can_successfully_cache_rates() {
     let request = GetExchangeRateRequest {
         timestamp: Some(timestamp),
         quote_asset: Asset {
-            symbol: "btc".to_string(),
+            symbol: "BTC".to_string(),
             class: AssetClass::Cryptocurrency,
         },
         base_asset: Asset {
-            symbol: "icp".to_string(),
+            symbol: "ICP".to_string(),
             class: AssetClass::Cryptocurrency,
         },
     };
