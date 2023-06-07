3a75df4eaf71b1bcf0dc19f812f8465e81eff588
Ryan Croote <ryan.croote@dfinity.org>
2023-06-07 21:01:42 +0000

    Updating routing

--- src/xrc-tests/src/mock_responses/forex.rs
+++ src/xrc-tests/src/mock_responses/forex.rs
@@ -2,22 +2,33 @@ use xrc::{Forex, FOREX_SOURCES};
 
 use crate::container::ExchangeResponse;
 
-mod bank_of_canada;
+mod austrailia;
+mod canada;
 
-pub fn build_responses(timestamp: u64) -> impl Iterator<Item = ExchangeResponse> + 'static {
+pub fn build_responses(now_timestamp: u64) -> impl Iterator<Item = ExchangeResponse> + 'static {
+    // Forex sources go back one day.
+    let yesterday_timestamp =
+        (now_timestamp.saturating_sub(86_400) / 86_400).saturating_mul(86_400);
     FOREX_SOURCES
         .iter()
-        .filter(|forex| matches!(forex, Forex::BankOfCanada(_)))
+        .filter(|forex| {
+            matches!(
+                forex,
+                Forex::BankOfCanada(_) | Forex::ReserveBankOfAustralia(_)
+            )
+        })
         .map(move |forex| {
-            let url = forex.get_url(timestamp);
+            let url = forex.get_url(yesterday_timestamp);
             let body = match forex {
                 Forex::MonetaryAuthorityOfSingapore(_) => todo!(),
                 Forex::CentralBankOfMyanmar(_) => todo!(),
                 Forex::CentralBankOfBosniaHerzegovina(_) => todo!(),
                 Forex::EuropeanCentralBank(_) => todo!(),
-                Forex::BankOfCanada(_) => bank_of_canada::build_response_body(timestamp),
+                Forex::BankOfCanada(_) => canada::build_response_body(yesterday_timestamp),
                 Forex::CentralBankOfUzbekistan(_) => todo!(),
-                Forex::ReserveBankOfAustralia(_) => todo!(),
+                Forex::ReserveBankOfAustralia(_) => {
+                    austrailia::build_response_body(yesterday_timestamp)
+                }
                 Forex::CentralBankOfNepal(_) => todo!(),
                 Forex::CentralBankOfGeorgia(_) => todo!(),
                 Forex::BankOfItaly(_) => todo!(),
---
+++ src/xrc-tests/src/mock_responses/forex/austrailia.rs
@@ -0,0 +1,125 @@
+use time::format_description;
+
+use crate::container::ResponseBody;
+
+const TEMPLATE: &str = r#"<?xml version="1.0" encoding="UTF-8"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rba="https://www.rba.gov.au/statistics/frequency/exchange-rates.html" xmlns:cb="http://www.cbwiki.net/wiki/index.php/Specification_1.2/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://purl.org/rss/1.0/" xsi:schemaLocation="http://www.w3.org/1999/02/22-rdf-syntax-ns# rdf.xsd">
+  <item rdf:about="https://www.rba.gov.au/statistics/frequency/exchange-rates.html#USD">
+    <cb:statistics rdf:parseType="Resource">
+      <cb:exchangeRate rdf:parseType="Resource">
+        <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+        <cb:observation rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:value>0.6677</cb:value>
+          <cb:unit>AUD</cb:unit>
+          <cb:decimals>4</cb:decimals>
+        </cb:observation>
+        <cb:targetCurrency>USD</cb:targetCurrency>
+        <cb:observationPeriod rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:frequency>daily</cb:frequency>
+          <cb:period>[DATE_STRING]</cb:period>
+        </cb:observationPeriod>
+      </cb:exchangeRate>
+    </cb:statistics>
+  </item>
+  <item rdf:about="https://www.rba.gov.au/statistics/frequency/exchange-rates.html#CNY">
+    <cb:statistics rdf:parseType="Resource">
+      <cb:exchangeRate rdf:parseType="Resource">
+        <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+        <cb:observation rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:value>4.5966</cb:value>
+          <cb:unit>AUD</cb:unit>
+          <cb:decimals>4</cb:decimals>
+        </cb:observation>
+        <cb:targetCurrency>CNY</cb:targetCurrency>
+        <cb:observationPeriod rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:frequency>daily</cb:frequency>
+          <cb:period>[DATE_STRING]</cb:period>
+        </cb:observationPeriod>
+      </cb:exchangeRate>
+    </cb:statistics>
+  </item>
+  <item rdf:about="https://www.rba.gov.au/statistics/frequency/exchange-rates.html#JPY">
+    <cb:statistics rdf:parseType="Resource">
+      <cb:exchangeRate rdf:parseType="Resource">
+        <cb:observation rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:value>88.98</cb:value>
+          <cb:unit>AUD</cb:unit>
+          <cb:decimals>2</cb:decimals>
+        </cb:observation>
+        <cb:targetCurrency>JPY</cb:targetCurrency>
+        <cb:observationPeriod rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:frequency>daily</cb:frequency>
+          <cb:period>[DATE_STRING]</cb:period>
+        </cb:observationPeriod>
+      </cb:exchangeRate>
+    </cb:statistics>
+  </item>
+  <item rdf:about="https://www.rba.gov.au/statistics/frequency/exchange-rates.html#EUR">
+    <cb:statistics rdf:parseType="Resource">
+      <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+      <cb:country>AU</cb:country>
+      <cb:institutionAbbrev>RBA</cb:institutionAbbrev>
+      <cb:exchangeRate rdf:parseType="Resource">
+        <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+        <cb:observation rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:value>0.6128</cb:value>
+          <cb:unit>AUD</cb:unit>
+          <cb:decimals>4</cb:decimals>
+        </cb:observation>
+        <cb:baseCurrency>AUD</cb:baseCurrency>
+        <cb:targetCurrency>EUR</cb:targetCurrency>
+        <cb:rateType>4.00 pm foreign exchange rates</cb:rateType>
+        <cb:observationPeriod rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:frequency>daily</cb:frequency>
+          <cb:period>[DATE_STRING]</cb:period>
+        </cb:observationPeriod>
+      </cb:exchangeRate>
+    </cb:statistics>
+  </item>
+  <item rdf:about="https://www.rba.gov.au/statistics/frequency/exchange-rates.html#GBP">
+    <cb:statistics rdf:parseType="Resource">
+      <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+      <cb:country>AU</cb:country>
+      <cb:institutionAbbrev>RBA</cb:institutionAbbrev>
+      <cb:exchangeRate rdf:parseType="Resource">
+        <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+        <cb:observation rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:value>0.5377</cb:value>
+          <cb:unit>AUD</cb:unit>
+          <cb:decimals>4</cb:decimals>
+        </cb:observation>
+        <cb:baseCurrency>AUD</cb:baseCurrency>
+        <cb:targetCurrency>GBP</cb:targetCurrency>
+        <cb:rateType>4.00 pm foreign exchange rates</cb:rateType>
+        <cb:observationPeriod rdf:parseType="Resource">
+          <rdf:type rdf:resource="http://www.cbwiki.net/wiki/index.php/RSS-CB_1.2_RDF_Schema#Exchange-Rates"/>
+          <cb:frequency>daily</cb:frequency>
+          <cb:period>[DATE_STRING]</cb:period>
+        </cb:observationPeriod>
+      </cb:exchangeRate>
+    </cb:statistics>
+  </item>
+</rdf:RDF>
+"#;
+
+pub fn build_response_body(timestamp: u64) -> ResponseBody {
+    let date = time::OffsetDateTime::from_unix_timestamp(timestamp as i64).expect(
+        "Failed to make date from given timestamp while build response for Reserve Bank of Austrailia.",
+    );
+    let format = format_description::parse("[year]-[month]-[day]")
+        .expect("Unable to determine time format for Reserve Bank of Austrailia.");
+    let date_string = date
+        .format(&format)
+        .expect("Failed to format date for Reserve Bank of Austrailia.");
+    let xml = TEMPLATE.replace("[DATE_STRING]", &date_string);
+    ResponseBody::Xml(xml.as_bytes().to_vec())
+}
